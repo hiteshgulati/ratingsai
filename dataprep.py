e956e31d44bf270b6dd7cf353a836afcac32e228
Hitesh Gulati <hg0111@gmail.com>
2018-01-04 02:26:00 +0530

    jan 3

--- dataprep.py
+++ dataprep.py
@@ -1,48 +1,80 @@
+# from bs4 import BeautifulSoup as soup
+# from urllib.request import urlopen as u_req
 import pandas as pd
-import os
-from difflib import SequenceMatcher
 
 
 def run():
-    # crisil_file_path = os.path.join(os.getcwd(),'crisil')
-    # list_of_alphabet_files = os.listdir(crisil_file_path)
-    # a_file = list_of_alphabet_files[0]
-    # df_complete = pd.read_csv(os.path.join(crisil_file_path, a_file))
-    # df_complete = df_complete.dropna()
-    #
-    # for each_alphabet_file in list_of_alphabet_files[1:]:
-    #     df = pd.read_csv(os.path.join(crisil_file_path,each_alphabet_file))
-    #     total_records = df.shape
-    #     df = df.dropna()
-    #     trimmed_records = df.shape
-    #     df_complete = df_complete.append(df)
-    #     print(each_alphabet_file, ":: ", total_records, "--> ", trimmed_records, "++--> ", df_complete.shape)
-    # print(df_complete.describe())
-    # df_complete.to_csv(path_or_buf=os.path.join(crisil_file_path,"aacompletefile.csv"), index=False)
-
-
+    string = "Sita Ram & Company"
+    # page_url = print (url_maker(string))
+    # get_containers(page_url)
 
     pass
 
+def make_table():
+    funda = pd.read_excel("data/funda.xlsx")
+    companies_list = pd.read_excel("data/final_list.xlsx")
+    compl = companies_list.Quandl_Code
+    slice = {"Company": "", "EBIDTMRG3": 0, "EBIDTMRG": 0, "REV3G": 0, "REV3SD": 0, "REV": 0, "TDGCA": 0, "GEARING": 0,
+             "GEARINGWT": 0, "CASH": 0, "TDCASH": 0, "CFTA": 0, "EQINF3": 0, "EQ3TNW": 0, "GCA": 0, "OPCYC": 0,
+             "ROCE": 0, "COB": 0, "EQ": 0, "TD": 0, "NP": 0, "RSTA": 0, "BWDR": 0, "CFO": 0, "CDS": 0}
+    for item in compl:
+        slice['Company'] = item
+        slice['EBIDTMRG3'] = (funda[(funda.Company == item) & (funda.Feature == "EBIDTPCT") &
+                                    (funda.Period == "t")].Adj_Value.iloc[0] +
+                              funda[(funda.Company == item) & (funda.Feature == "EBIDTPCT") & (
+                              funda.Period == "t-1")].Adj_Value.iloc[0] +
+                              funda[(funda.Company == item) & (funda.Feature == "EBIDTPCT") &
+                                    (funda.Period == "t-2")].Adj_Value.iloc[0])/3
+        slice['EBIDTMRG'] = funda[(funda.Company == item) & (funda.Feature == "EBIDTPCT") & (funda.Period == "t")].Adj_Value.iloc[0]
+        slice['REV3G'] = funda[(funda.Company == item) & (funda.Feature == "REV3") &
+                               (funda.Period == "t")].Adj_Value.iloc[0]
 
-def company_matching(threshold= .8):
-    file_path = os.getcwd()
-    file_name = "crisil ratings.xlsx"
-    crisil_df = pd.read_excel(os.path.join(file_path,file_name), sheetname="allcrisil")
-    nse_df = pd.read_excel(os.path.join(file_path,file_name), sheetname="allnse")
-    combined_df = pd.DataFrame(columns=list(crisil_df))
-    combined_df['company_nse'] = None
-    combined_df['code_nse'] = None
-    unused_crisil = pd.DataFrame(columns=list(crisil_df))
-    unused_nse = pd.DataFrame(columns=list(nse_df))
-    for index_nse, item_nse in nse_df.iterrows():
-        for index_crisil, item_crisil in crisil_df.iterrows():
-            if SequenceMatcher(None,item_nse['company_nse'], item_crisil['company']).ratio() >= threshold:
-                item_crisil['company_nse'] = item_nse['company_nse']
-                item_crisil['code_nse'] = item_nse['code_nse']
+        pass
 
 
 
+
+def get_listed_companies(containers):
+    c = {}
+    flag = 0
+    cin = ""
+    for item_tag in containers:
+        item = str(item_tag)
+        if flag == 1:
+            c[cin] = item[4:-5]
+            flag = 0
+        if item[:5] == "<h5>L" and len(item) == 30 :
+            cin = item[4:-5]
+            flag = 1
+    return c
+
+def create_list ():
+    company_list = pd.read_excel("data/final_list.xlsx")
+    for i, index in company_list.iterrows():
+
+
+
+
+def get_containers(page_url):
+    u_client = u_req(page_url, timeout=10)
+    page_html = u_client.read()
+    u_client.close()
+    page_soup = soup(page_html, 'html.parser')
+    containers = page_soup.findAll("h5")
+    return containers
+
+def url_maker(search_string):
+    base_url = "https://www.zaubacorp.com/companysearchresults/"
+    search_string_url = ""
+    for e in search_string:
+        if e.isalnum():
+            search_string_url += e.lower()
+        else:
+            search_string_url += "-"
+    search_url = base_url + search_string_url
+    return search_url
+
+    pass
 
 
 
